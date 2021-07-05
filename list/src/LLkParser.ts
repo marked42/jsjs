088f6a49f94cec6d05f8e71df6b4525cdb091480
penghui <penghui.zhang@foxmail.com>
2021-07-06 01:20:48 +0800

    feat: list lang llk parser

--- list/src/LLkParser.ts
+++ list/src/LLkParser.ts
@@ -1,10 +1,57 @@
 import { Lexer } from './Lexer'
-import { Parser } from './Parser'
-import { TokenType } from './Token'
+import { Token, TokenType } from './Token'
 
-export class LLkParser extends Parser {
-  constructor(input: Lexer) {
-    super(input)
+export class LLkParser {
+  private lookaheadTokens!: Token[]
+  private index = 0
+  private lookaheadCount = 0
+
+  constructor(private input: Lexer, private size: number) {
+    if (size < 1) {
+      throw new Error('lookahead size must be an integer bigger than 0 ')
+    }
+    this.lookaheadTokens = new Array<Token>(size)
+  }
+
+  private lookaheadIndex(i: number) {
+    return (this.index + i - 1) % this.size
+  }
+
+  lookahead(i: number) {
+    if (i > this.size) {
+      throw new Error(
+        `try to lookahead next ${i} token, but max lookahead is ${this.size}`
+      )
+    }
+    if (this.lookaheadCount >= i) {
+      return this.lookaheadTokens[this.lookaheadIndex(i)]
+    }
+    this.fill(i)
+
+    return this.lookaheadTokens[this.lookaheadIndex(i)]
+  }
+
+  private fill(i: number) {
+    for (let j = this.lookaheadCount + 1; j <= i; j++) {
+      this.lookaheadTokens[this.lookaheadIndex(j)] = this.input.nextToken()
+    }
+    this.lookaheadCount = i
+  }
+
+  match(type: TokenType) {
+    if (this.lookahead(1).type !== type) {
+      throw new Error(
+        `unexpected token type, expecting ${type}, get ${this.lookahead(1)}`
+      )
+    }
+
+    this.consume()
+  }
+
+  consume() {
+    // 前进到下一个位置
+    this.index = this.lookaheadIndex(2)
+    this.lookaheadCount--
   }
 
   list() {
@@ -15,16 +62,23 @@ export class LLkParser extends Parser {
 
   elements() {
     this.element()
-    while (this.lookahead.type === TokenType.Comma) {
+    while (this.lookahead(1).type === TokenType.Comma) {
       this.consume()
       this.element()
     }
   }
 
   element() {
-    if (this.lookahead.type === TokenType.Name) {
+    if (
+      this.lookahead(1).type === TokenType.Name &&
+      this.lookahead(2).type === TokenType.Equal
+    ) {
       this.match(TokenType.Name)
-    } else if (this.lookahead.type === TokenType.LeftBracket) {
+      this.match(TokenType.Equal)
+      this.match(TokenType.Name)
+    } else if (this.lookahead(1).type === TokenType.Name) {
+      this.match(TokenType.Name)
+    } else if (this.lookahead(1).type === TokenType.LeftBracket) {
       this.list()
     } else {
       throw new Error(`expecting name or list, found ${this.lookahead}`)
