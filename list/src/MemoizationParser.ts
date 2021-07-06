1dfa8e6f25af1d12b10dba03f0579c869c1ef268
penghui <penghui.zhang@foxmail.com>
2021-07-07 00:52:42 +0800

    feat: packrat parsing, backtrack with memoization

---
+++ list/src/MemoizationParser.ts
@@ -0,0 +1,165 @@
+import { Lexer } from './Lexer'
+import { Token, TokenType } from './Token'
+
+export class MemoizationParser {
+  private tokens: Token[] = []
+  private index = 0
+  private markers: number[] = []
+
+  constructor(private input: Lexer) {}
+
+  private fill(i: number) {
+    for (let j = this.tokens.length; j <= this.index + i - 1; j++) {
+      this.tokens[j] = this.input.nextToken()
+    }
+  }
+
+  lookahead(i: number) {
+    this.fill(i)
+    return this.tokens[this.index + i - 1]
+  }
+
+  match(type: TokenType) {
+    if (this.lookahead(1).type !== type) {
+      throw new Error(
+        `unexpected token type, expecting ${type}, get ${this.lookahead(1)}`
+      )
+    }
+
+    this.consume()
+  }
+
+  isSpeculating() {
+    return this.markers.length > 0
+  }
+
+  consume() {
+    if (this.index === this.tokens.length - 1 && !this.isSpeculating()) {
+      this.index = 0
+      this.tokens = []
+      this.clearMemo()
+    } else {
+      this.index++
+    }
+  }
+
+  clearMemo() {
+    this.listMemo.clear()
+  }
+
+  mark() {
+    this.markers.push(this.index)
+  }
+
+  release() {
+    const value = this.markers.pop()
+    if (value === undefined) {
+      throw new Error('no marker to release')
+    }
+    this.index = value
+  }
+
+  speculate(rule: () => void) {
+    let success = false
+    this.mark()
+    try {
+      rule()
+      success = true
+    } catch (e) {
+      success = false
+    }
+    this.release()
+
+    return success
+  }
+
+  stat() {
+    if (this.speculate(this.stat_alt1.bind(this))) {
+      this.stat_alt1()
+    } else if (this.speculate(this.stat_alt2.bind(this))) {
+      this.stat_alt2()
+    } else {
+      throw new Error('no viable alternative rule for stat')
+    }
+  }
+
+  stat_alt1() {
+    this.list()
+    this.match(TokenType.EOF)
+  }
+
+  stat_alt2() {
+    this.assign()
+    this.match(TokenType.EOF)
+  }
+
+  assign() {
+    this.list()
+    this.match(TokenType.Equal)
+    this.list()
+  }
+
+  private static FAILED = -1
+  alreadyParsedRule(memo: Map<number, number>) {
+    const value = memo.get(this.index)
+
+    if (value === undefined) {
+      return false
+    }
+
+    if (value === MemoizationParser.FAILED) {
+      throw new Error(`unexpected token ${this.lookahead(1)}`)
+    }
+
+    this.index = value
+    return true
+  }
+
+  list() {
+    const start = this.index
+    if (this.isSpeculating() && this.alreadyParsedRule(this.listMemo)) {
+      return
+    }
+
+    let pos = MemoizationParser.FAILED
+    try {
+      this._list()
+      pos = this.index
+    } finally {
+      this.listMemo.set(start, pos)
+    }
+  }
+
+  private listMemo = new Map<number, number>()
+
+  _list() {
+    this.match(TokenType.LeftBracket)
+    this.elements()
+    this.match(TokenType.RightBracket)
+  }
+
+  elements() {
+    this.element()
+    while (this.lookahead(1).type === TokenType.Comma) {
+      this.match(TokenType.Comma)
+      this.element()
+    }
+  }
+
+  element() {
+    if (
+      this.lookahead(1).type === TokenType.Name &&
+      this.lookahead(2).type === TokenType.Equal
+    ) {
+      this.match(TokenType.Name)
+      this.match(TokenType.Equal)
+      this.match(TokenType.Name)
+    } else if (this.lookahead(1).type === TokenType.Name) {
+      this.match(TokenType.Name)
+    } else if (this.lookahead(1).type === TokenType.LeftBracket) {
+      this.list()
+    } else {
+      throw new Error('no viable alternative for rule element')
+    }
+  }
+}
