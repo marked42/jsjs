4c38b48cddba45353bb5879019ce32caeacdc1a8
penghui <zhangpenghui04@meituan.com>
2021-07-11 16:33:06 +0800

    feat: closure

--- jlox/jlox/src/main/java/com/kos/language/LoxFunction.java
+++ jlox/jlox/src/main/java/com/kos/language/LoxFunction.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 public class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
+    private final Environment closure;
 
-    LoxFunction(Stmt.Function declaration) {
+    LoxFunction(Stmt.Function declaration, Environment environment) {
         this.declaration = declaration;
+        this.closure = environment;
     }
 
     @Override
@@ -17,7 +19,7 @@ public class LoxFunction implements LoxCallable {
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
         // 使用当前Environment
-        Environment environment = new Environment(interpreter.globals);
+        Environment environment = new Environment(closure);
         for (int i = 0; i < declaration.params.size(); i++) {
             Token name = declaration.params.get(i);
             environment.define(name.lexeme, arguments.get(i));
