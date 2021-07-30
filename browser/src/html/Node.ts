39ef231e77e2f58d582f887bef32b69295a5ba22
penghui <zhangpenghui04@meituan.com>
2021-07-30 18:12:14 +0800

    feat: parse html

--- browser/src/html/Node.ts
+++ browser/src/html/Node.ts
@@ -7,20 +7,24 @@ export interface TextNode {
   type: NodeType.Text
 }
 
+export interface NodeAttributeMap {
+  [key: string]: string
+}
+
 export abstract class Node {
   constructor(public type: NodeType) {}
 }
 
 export class TextNode extends Node {
-  constructor(public textContent: string) {
+  constructor(public value: string) {
     super(NodeType.Text)
   }
 }
 
 export class ElementNode extends Node {
   constructor(
-    public name: string,
-    public attributes: Record<string, string>,
+    public tag: string,
+    public attributes: NodeAttributeMap,
     public children: Node[]
   ) {
     super(NodeType.Element)
--- browser/test/html/HTMLParser.test.ts
+++ browser/test/html/HTMLParser.test.ts
@@ -3,8 +3,13 @@ import { parseHTML } from '../../src/html'
 describe('parse html', () => {
   it('should parse html', () => {
     const input = `
-			<div>hello</div>
+			<div test="a">hello</div>
 		`
-    console.log(parseHTML(input))
+    expect(parseHTML(input)).toMatchSnapshot()
+  })
+
+  it('should parse html', () => {
+    const input = `<html><body>Hello, world!</body></html>`
+    expect(parseHTML(input)).toMatchSnapshot()
   })
 })
